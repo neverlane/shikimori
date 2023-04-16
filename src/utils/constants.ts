71321c43bf204377f3aa749082e607ed860ce693
neverlane <naer200469@gmail.com>
2023-04-16 12:24:01 +0300

    BREAKING CHANGES: changed shikimori domain (.one -> .me)

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,7 @@ import { name, version } from '../../package.json';
 export const RUNNING_IN_BROWSER = typeof window !== 'undefined';
 export const RUNNING_IN_NODE = !RUNNING_IN_BROWSER;
 
-export const SHIKIMORI_URL = 'https://shikimori.one';
+export const SHIKIMORI_URL = 'https://shikimori.me';
 export const SHIKIMORI_API_URL = SHIKIMORI_URL + '/api';
 export const SHIKIMORI_OAUTH_URL = SHIKIMORI_URL + '/oauth';
 export const API_USER_AGENT = `${name}/${version} (${RUNNING_IN_NODE ? `NodeJS ${process.version}` : 'Browser'})`;
