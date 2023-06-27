789a4c49f8a87b02cfe80d27cab0322b6e5db92e
neverlane <naer200469@gmail.com>
2023-06-27 21:27:35 +0300

    feat(types): add ShikimoriNotValue

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,11 +1,12 @@
-export type AllowArray<T> = T | T[]
+export type AllowArray<T> = T | T[];
 export type Nullable<T> = T | null;
 export type NullableArray<T> = T[] | [null];
 export type DateString = string;
 export type Without<T extends string, K extends T> = T extends K ? never : T;
 
-export type ShikimoriParameterType = number | string
-export type ShikimoriParameter = AllowArray<ShikimoriParameterType>
+export type ShikimoriParameterValueType = number | string;
+export type ShikimoriNotValue<T extends number | string> = `!${T}`;
+export type ShikimoriParameter = AllowArray<ShikimoriNotValue<ShikimoriParameterValueType>>;
 
 export type ShikimoriOrder = 'id' | 'id_desc' | 'ranked' | 'kind' | 'popularity' | 'name' | 'aired_on' | 'episodes' | 'status' | 'random' | 'ranked_random' | 'ranked_shiki' | 'created_at' | 'created_at_desc';
 export type ShikimoriRating = 'none' | 'g' | 'pg' | 'pg_13' | 'r' | 'r_plus' | 'rx';
